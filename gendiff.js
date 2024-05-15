c7d65fe5776afb14dba5e5f04659a2f53e1b9ed7
luka0204 <Luka9609@gmail.com>
2024-05-15 13:03:43 +0300

    3/9

--- gendiff.js
+++ gendiff.js
@@ -1,5 +1,6 @@
-const { Command } = require('commander');
+import { Command } from 'commander';
 const program = new Command();
+import { readFileSync } from 'node:fs';
 
 program
   .name('gendiff')
@@ -7,4 +8,11 @@ program
   .version('1.0.0')
   .option('-f, --format [type]', 'output format')
   .usage("[options] <filepath1> <filepath2>")
+  .argument('<filepath1>')
+  .argument('<filepath2>')
+.action((filepath1,filepath2, format = 'utf-8') => {
+const result1 = readFileSync(filepath1, format);
+const result2 = readFileSync(filepath2, format);
+console.log(`${result1} \n ${result2}`);
+});
 program.parse();
