e36f73b8c4ad36b707e57363208a5ea199563ef4
luka0204 <Luka9609@gmail.com>
2024-05-15 13:30:56 +0300

    4/9

--- gendiff.js
+++ gendiff.js
@@ -1,6 +1,7 @@
 import { Command } from 'commander';
 const program = new Command();
 import { readFileSync } from 'node:fs';
+import _ from 'lodash';
 
 program
   .name('gendiff')
@@ -13,6 +14,24 @@ program
 .action((filepath1,filepath2, format = 'utf-8') => {
 const result1 = readFileSync(filepath1, format);
 const result2 = readFileSync(filepath2, format);
-console.log(`${result1} \n ${result2}`);
+
+const genDiff = (data1, data2) => {
+  const keys = _.union(Object.keys(data1), Object.keys(data2)).sort();
+  const diffLines = keys.map((key) => {
+    if (!_.has(data1, key)) {
+      return `+ ${key}: ${data2[key]}`;
+    }
+    if (!_.has(data2, key)) {
+      return `- ${key}: ${data1[key]}`;
+    }
+    if (data1[key] === data2[key]) {
+      return `  ${key}: ${data1[key]}`;
+    }
+    return `- ${key}: ${data1[key]}\n+ ${key}: ${data2[key]}`;
+  });
+  
+  return `{\n${diffLines.join('\n')}\n}`;
+};
+console.log(genDiff(JSON.parse(result1), JSON.parse(result2)));
 });
 program.parse();
