8b4337a1b3e8f7950359356000ebc0e6d02f9344
luka0204 <Luka9609@gmail.com>
2024-05-22 12:58:05 +0300

    7/9 latest

--- formatter.js
+++ formatter.js
@@ -30,6 +30,43 @@ const formatValue = (value, depth) => {
     return value;
 };
 
+const makePlainDiff = (obj1, obj2) => {
+    const diffOutput = [];
+
+    const formatValue = (value) => {
+        if (_.isObject(value)) {
+            return '[complex value]';
+        }
+        return `"${value}"`;
+    };
+
+    Object.keys(obj1).forEach((key) => {
+        if (!obj2.hasOwnProperty(key)) {
+            diffOutput.push(`Property '${key}' was removed`);
+        } else if (_.isEqual(obj1[key], obj2[key])) {
+            // Property exists in both objects and their values are equal
+            // No action needed
+        } else if (_.isPlainObject(obj1[key]) && _.isPlainObject(obj2[key])) {
+            const nestedDiffOutput = makePlainDiff(obj1[key], obj2[key]);
+            if (nestedDiffOutput.length > 0) {
+                nestedDiffOutput.forEach((line) => {
+                    diffOutput.push(`Property '${key}.${line.split("'")[1]}' ${line.split("'")[2]}`);
+                });
+            }
+        } else {
+            diffOutput.push(`Property '${key}'was updated. From ${formatValue(obj1[key])} to ${formatValue(obj2[key])}`);
+        }
+    });
+
+    Object.keys(obj2).forEach((key) => {
+        if (!obj1.hasOwnProperty(key)) {
+            diffOutput.push(`Property '${key}' was added with value: ${formatValue(obj2[key])}`);
+        }
+    });
+
+    return diffOutput;
+};
+
 const formatter = (data1,data2,format) => {
         switch (format) {
           case 'stylish':
