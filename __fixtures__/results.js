04c13860e5f7adbec42697b55b28fd0af3c49c4e
luka0204 <Luka9609@gmail.com>
2024-05-24 14:13:12 +0300

    9/9 test 3

---
+++ __fixtures__/results.js
@@ -0,0 +1,118 @@
+export const expectedPlaintOutput = `{
+    Property 'common.follow' was added with value: false
+    Property 'common.setting2' was removed
+    Property 'common.setting3' was updated. From true to null
+    Property 'common.setting4' was added with value: 'blah blah'
+    Property 'common.setting5' was added with value: [complex value]
+    Property 'common.setting6.doge.wow' was updated. From '' to 'so much'
+    Property 'common.setting6.ops' was added with value: 'vops'
+    Property 'group1.baz' was updated. From 'bas' to 'bars'
+    Property 'group1.nest' was updated. From [complex value] to 'str'
+    Property 'group2' was removed
+    Property 'group3' was added with value: [complex value]
+}`;
+
+export const expectedStylishOutput = `{
+  common: {
+    + follow: false
+      setting1: Value 1
+    - setting2: 200
+    - setting3: true
+    + setting3: null
+    + setting4: blah blah
+    + setting5: {
+          key5: value5
+      }
+      setting6: {
+          doge: {
+            - wow: 
+            + wow: so much
+          }
+          key: value
+        + ops: vops
+      }
+  }
+  group1: {
+    - baz: bas
+    + baz: bars
+      foo: bar
+    - nest: {
+          key: value
+      }
+    + nest: str
+  }
+- group2: {
+      abc: 12345
+      deep: {
+          id: 45
+      }
+  }
++ group3: {
+      deep: {
+          id: {
+              number: 45
+          }
+      }
+      fee: 100500
+  }
+}`;
+
+export const obj1 = {
+   "common": {
+     "setting1": "Value 1",
+     "setting2": 200,
+     "setting3": true,
+     "setting6": {
+       "key": "value",
+       "doge": {
+         "wow": ""
+       }
+     }
+   },
+   "group1": {
+     "baz": "bas",
+     "foo": "bar",
+     "nest": {
+       "key": "value"
+     }
+   },
+   "group2": {
+     "abc": 12345,
+     "deep": {
+       "id": 45
+     }
+   }
+};
+   
+export const obj2 = {
+    "common": {
+      "follow": false,
+      "setting1": "Value 1",
+      "setting3": null,
+      "setting4": "blah blah",
+      "setting5": {
+        "key5": "value5"
+      },
+      "setting6": {
+        "key": "value",
+        "ops": "vops",
+        "doge": {
+          "wow": "so much"
+        }
+      }
+    },
+    "group1": {
+      "foo": "bar",
+      "baz": "bars",
+      "nest": "str"
+    },
+    "group3": {
+      "deep": {
+        "id": {
+          "number": 45
+        }
+      },
+      "fee": 100500
+    }
+};
+
--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,6 +1,7 @@
 import { describe, expect, it } from '@jest/globals';
 import formatter from '../formatter.js';
 import * as func from '../formatter.js';
+import * as example from '../__fixtures__/results.js';
 
  describe('genDiff', () => {
    it('should be undefined', () => {
@@ -11,111 +12,9 @@ import * as func from '../formatter.js';
 
  describe('genDiff', () => {
      test('should generate the correct diff output with proper indentation', () => {
-         const obj1 = {
-          "common": {
-            "setting1": "Value 1",
-            "setting2": 200,
-            "setting3": true,
-            "setting6": {
-              "key": "value",
-              "doge": {
-                "wow": ""
-              }
-            }
-          },
-          "group1": {
-            "baz": "bas",
-            "foo": "bar",
-            "nest": {
-              "key": "value"
-            }
-          },
-          "group2": {
-            "abc": 12345,
-            "deep": {
-              "id": 45
-            }
-          }
-         };
-         
-         const obj2 = {
-          "common": {
-            "follow": false,
-            "setting1": "Value 1",
-            "setting3": null,
-            "setting4": "blah blah",
-            "setting5": {
-              "key5": "value5"
-            },
-            "setting6": {
-              "key": "value",
-              "ops": "vops",
-              "doge": {
-                "wow": "so much"
-              }
-            }
-          },
-          "group1": {
-            "foo": "bar",
-            "baz": "bars",
-            "nest": "str"
-          },
-          "group3": {
-            "deep": {
-              "id": {
-                "number": 45
-              }
-            },
-            "fee": 100500
-          }
-         };
- 
-  const expectedDiffOutput = `{{
-    common: {
-      + follow: false
-        setting1: Value 1
-      - setting2: 200
-      - setting3: true
-      + setting3: null
-      + setting4: blah blah
-      + setting5: {
-            key5: value5
-        }
-        setting6: {
-            doge: {
-              - wow: 
-              + wow: so much
-            }
-            key: value
-          + ops: vops
-        }
-    }
-    group1: {
-      - baz: bas
-      + baz: bars
-        foo: bar
-      - nest: {
-            key: value
-        }
-      + nest: str
-    }
-  - group2: {
-        abc: 12345
-        deep: {
-            id: 45
-        }
-    }
-  + group3: {
-        deep: {
-            id: {
-                number: 45
-            }
-        }
-        fee: 100500
-    }
-}}`
-;
- 
-         expect(formatter(obj1, obj2, 'stylish')).toEqual(expectedDiffOutput);
+
+         expect(formatter(example.obj1, example.obj2, 'stylish')).toEqual(example.expectedStylishOutput);
+        //  expect(formatter(obj1, obj2, 'plain')).toEqual(expectedPlainOutput);
+        //  expect(formatter(obj1, obj2, 'json')).toEqual(expectedJsonOutput);
      });
  });
